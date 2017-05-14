e524eca28b164a733a14d6d47d6bc7ec7125dc46
carlo <carlo@localhost.localdomain>
2017-05-13 20:00:01 -0500

    create item and spider example

--- tasi/tasi/items.py
+++ tasi/tasi/items.py
@@ -7,8 +7,5 @@
 
 import scrapy
 
-
-class TasiItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class androidCentralItem(scrapy.Item):
+  title = scrapy.Field()
